bf9c86b314a1e2fab12808673f7e84bf05823939
akristianlopez <30592645+akristianlopez@users.noreply.github.com>
2026-03-13 21:40:52 +0100

    Update ast
    Update ast

--- ast/ast.go
+++ ast/ast.go
@@ -193,7 +193,9 @@ func (sl *StringLiteral) expressionNode()      {}
 func (sl *StringLiteral) Line() int            { return sl.Token.Line }
 func (sl *StringLiteral) Column() int          { return sl.Token.Column }
 func (sl *StringLiteral) TokenLiteral() string { return sl.Token.Literal }
-func (sl *StringLiteral) String() string       { return "\"" + sl.Value + "\"" }
+func (sl *StringLiteral) String() string {
+	return fmt.Sprintf("'%s'", strings.ReplaceAll(sl.Value, "'", "''"))
+}
 
 // BooleanLiteral - littéral booléen
 type BooleanLiteral struct {

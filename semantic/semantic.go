4063033e1ae0ea7326b77b269e918f16f086f546
akristianlopez <30592645+akristianlopez@users.noreply.github.com>
2026-03-17 08:51:13 +0100

    Update semantic
    We updated semantic to take into account the alias of integer from postgres.

--- semantic/semantic.go
+++ semantic/semantic.go
@@ -2760,13 +2760,13 @@ func (sa *SemanticAnalyzer) formType(col *sql.ColumnType) *TypeInfo {
 	s = strings.ReplaceAll(s, "double", "float")
 	s = strings.ReplaceAll(s, "blob", "string")
 	switch s {
-	case "int2":
+	case "int2", "integer2":
 		return &TypeInfo{Name: "integer", Constraints: &Constraint{Length: 5, Scale: -1, Precision: -1,
 			Range: &RangeValue{Min: -32768, Max: +32767}}}
-	case "int4":
+	case "int4", "integer4":
 		return &TypeInfo{Name: "integer", Constraints: &Constraint{Length: 10, Scale: -1, Precision: -1,
 			Range: &RangeValue{Min: -2147483648, Max: +2147483647}}}
-	case "int8":
+	case "int8", "integer8":
 		return &TypeInfo{Name: "integer", Constraints: &Constraint{Length: 19, Scale: -1, Precision: -1,
 			Range: &RangeValue{Min: -9223372036854775808, Max: +9223372036854775807}}}
 	}

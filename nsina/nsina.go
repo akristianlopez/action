08440e4d7348fbe079f9e0630db06eaed5680dfc
akristianlopez <30592645+akristianlopez@users.noreply.github.com>
2026-03-18 23:19:06 +0100

    Update nsina and object
    Update nsina and object

--- nsina/nsina.go
+++ nsina/nsina.go
@@ -33,7 +33,7 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 	case *ast.FloatLiteral:
 		return &object.Float{Value: node.Value}
 	case *ast.StringLiteral:
-		return &object.String{Value: node.Value}
+		return &object.String{Value: fmt.Sprintf("'%s'", node.Value)}
 	case *ast.BooleanLiteral:
 		return &object.Boolean{Value: node.Value}
 	case *ast.DateTimeLiteral:
